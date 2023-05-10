6dd483d2b36dbbfb980167d70ee0cc3d46b8771e
José Jorge Díaz <Jdiaz@somosproper.com>
2023-05-10 08:00:50 -0600

    Cambios

--- script.sh
+++ script.sh
@@ -2,19 +2,19 @@
 echo "--------------------PRUEBA DE FUNCIONAMIENTO-------------------------------"
 
 cd "$(dirname "$(readlink -f "$0")")"
-	
-	while IFS= read -r line
-	do	
-		echo $line'/*'
-		nombre=$(basename $line)
-		echo 'DIrectorio:' $nombre
-		
-		rm -r $line/*
-		##for archivo in base/*
-		##do
-			##echo 'ARCHIVO A COPIAR: ' $archivo
-			cp -r base/* "$line"
-			
-		##done
-		sed -i "s/\"\//\"\/$nombre\//" $line/index.html
-	done < clientes0
+
+while IFS= read -r line; do
+  # Si la línea está en blanco o sólo contiene espacios, no hacemos nada
+  if [[ -z "${line// }" ]]; then
+    echo "La línea está vacía, no se eliminan archivos."
+  else
+    echo "$line/*"
+    nombre=$(basename "$line")
+    echo "Directorio: $nombre"
+  
+    rm -r "$line"/*
+    cp -r base/* "$line"
+  
+    sed -i "s/\"\//\"\/$nombre\//" "$line/index.html"
+  fi
+done < clientes0
